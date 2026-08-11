fcf82eed4b36da9790826348ada6808cb38e61c0
mao-test-h <miymao78.d@gmail.com>
2026-08-11 19:06:16 +0900

    iOS 27向けMetricsKitサンプルを追加

---
+++ Assets/_Example/2_MetalPerformanceMetrics/Runtime/ExampleApplication.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace _Example.MetalPerformanceMetrics
+{
+    internal sealed class ExampleApplication : MonoBehaviour
+    {
+        [SerializeField] private UIDocument uiDocument;
+    }
+}
